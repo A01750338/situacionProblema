4ed448648b98d12b29e186a2c768f33dfba39e4b
A01750338 <A01750338@tec.mx>
2023-06-14 11:40:04 -0600

    Clase Video
    Se declaró e implementó la clase Video

---
+++ Video.cpp
@@ -0,0 +1,54 @@
+/**
+ * Autor: Min Che Kim
+ * Implementacion de la clase Video
+*/
+#include "Video.h"
+
+Video::Video(string id, string nombre, int duracion, string genero, double calif, string fecha)
+{
+}
+
+string Video::getID()
+{
+    return string();
+}
+
+string Video::getNombre()
+{
+    return string();
+}
+
+int Video::getDuracion()
+{
+    return 0;
+}
+
+string Video::getGenero()
+{
+    return string();
+}
+
+double Video::getCalif()
+{
+    return 0.00;
+}
+
+string Video::getFecha()
+{
+    return string();
+}
+
+void Video::mostrarPeliculas()
+{
+}
+
+void Video::mostrarEp()
+{
+}
+
+/*
+void Video::mostrarCal()
+{
+    
+}
+*/
--- Video.h
+++ Video.h
@@ -1,1 +1,37 @@
-// rmr
+/**
+ * Autor: Min Che Kim
+ * Clase Video 
+*/
+
+#ifndef VIDEO_H
+#define VIDEO_H
+
+#include <string>
+using namespace std;
+
+class Video
+{
+    protected:
+        string id;
+        string nombre;
+        string genero;
+        string fecha;
+        int duracion;
+        double calif;
+    public:
+        Video(string id, string nombre, int duracion, string genero, double calif, string fecha);
+        void leerArchivo();
+        string getID();
+        string getNombre();
+        string getGenero();
+        string getFecha();
+        int getDuracion();
+        double getCalif();
+        void mostrarPeliculas();
+        void mostrarEp();
+        //void mostrarCal();
+        virtual void calificar(double calN) = 0;
+
+};
+
+#endif
